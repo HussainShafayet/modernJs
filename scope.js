77fbbcaa6f75f1dd128d61c3a990edb8f74648db
shafayet <shafayetsec09@gmail.com>
2024-05-09 12:04:26 +0600

    scope practise

---
+++ scope.js
@@ -0,0 +1,12 @@
+let a = 10;
+const outer = ()=>{
+    let b = 15;
+    const inner = ()=>{
+        let c = 20;
+        console.log(a, b, c);
+    }
+    console.log('c', c);
+    inner();
+}
+console.log('b', b);
+outer();
