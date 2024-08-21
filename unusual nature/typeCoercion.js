8764240756632326f9179f0c1a799282d8da4d53
HussainShafayet <shafayetsec09@gmail.com>
2024-08-22 00:59:12 +0600

    type coercion practise

--- unusual nature/typeCoercion.js
+++ unusual nature/typeCoercion.js
@@ -3,3 +3,7 @@ console.log([] == ![]);
 //empty erray is coerced empty string , number, or boolean like premetive value
 
 console.log([] === ![]);
+
+const a = 4;
+const b = '6';
+console.log(a+b);
